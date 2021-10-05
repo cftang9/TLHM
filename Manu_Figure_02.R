941754094d915770451cc9d4bd8a06c981b84158
cftang9 <45774851+cftang9@users.noreply.github.com>
2021-10-04 20:55:04 -0500

    Add files via upload

--- Manu_Figure_02.R
+++ Manu_Figure_02.R
@@ -1,22 +1,14 @@
 rm(list=ls(all=TRUE))
 library(stabledist)
-library(fBasics)
-library(StableEstim)
-library(libstableR)
-library(evd)
-library(extremefit)
 library(xtable)
 source("TL_Library.R")
 
-n = 10^6; 
+n = 10^6; alpha = 0.50
 
 B = 1000; 
+M = array(,B); N.Upper = M; 
 
-alpha = 0.50
-
-
-
-M = array(,B); V=M; V.Upper=M; V.Local.Upper=M; V.Lower=M; N.Upper=M; 
+### Generating random samples from F(1,\alpha) and calculate the number of observations exceeding the sample mean
 st.time = Sys.time()
 set.seed(03282021)
 for(b in 1:B){
@@ -25,8 +17,6 @@ for(b in 1:B){
               beta=1, 
               gamma=abs(1-complex(imaginary=1)*tan(pi*alpha/2))^(-1/alpha), 
               delta=0,pm=1)
-  #X = rfrechet(n,loc=0,scale=1,shape=alpha)
-  #X = rpareto(n, a=alpha, loc=0, scale=1)
   in.time = (Sys.time()-st.time)/b
   N.Upper[b] = Num.Upper(X)
   if(b%%100==0){
@@ -36,12 +26,8 @@ for(b in 1:B){
   }
 }
 
-pdf(file = "050pp.pdf",   # The directory you want to save the file in
-    width = 11, # The width of the plot in inches
-    height = 5)
-
+### Drawing Figure 1 
 par(mfrow=c(1,2))
-#par(mar=c(4,4,2,1))
 hist(N.Upper/n^(alpha), col="red", 20,
      ylim = c(0,B), 
      xlab="", 
@@ -49,30 +35,23 @@ hist(N.Upper/n^(alpha), col="red", 20,
        paste("Histogram of ", U^-alpha/Gamma(1-alpha), " and ", N[n]^"+"/n^alpha))
 )
 set.seed(03282021)
+###### Generating random sample U_1,...U_B from F(1,\alpha). 
 U = rstable(B,alpha=alpha, 
             beta=1, 
             gamma=abs(1-complex(imaginary=1)*tan(pi*alpha/2))^(-1/alpha), 
             delta=0,pm=1)
 hist(U^(-alpha)/gamma(1-alpha), add=T, col=rgb(0, 1, 0, 0.5))
-legend("topright", # places a legend at the appropriate place c(“Health”,”Defense”), # puts text in the legend
+legend("topright", 
        c(expression(U^-alpha/Gamma(1-alpha)), expression(N[n]^"+"/n^alpha)), 
-       lty=c(1,1), # gives the legend appropriate symbols (lines)
+       lty=c(1,1), 
        lwd=c(2.5,2.5),col=c(rgb(0, 1, 0, 0.5),"red"))
-
-
-
+###### Performing two-sample Kolmogorov–Smirnov test
 ks.test(N.Upper/n^(alpha), U^(-alpha)/gamma(1-alpha))
-
-ks.test(log(N.Upper/n^(alpha)), log(U^(-alpha)/gamma(1-alpha)))
-
+###### QQ plots for N.Upper/n^alpha versus U^(-alpha)/gamma(1-alpha)
 plot((sort(N.Upper/n^(alpha))), (sort(U^(-alpha)/gamma(1-alpha))), 
      xlab = expression(paste(N[n]^"+"/n^alpha)),
      ylab = expression(paste(U^-alpha/Gamma(1-alpha))),
      main = expression(
        paste("qqplot of ", U^-alpha/Gamma(1-alpha), " and ", N[n]^"+"/n^alpha)))
 abline(0,1,lty=3)
-
 par(mfrow=c(1,1))
-
-dev.off()
-
